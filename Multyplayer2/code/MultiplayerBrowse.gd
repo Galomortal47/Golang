228eafdf5631e84dfd99da5e4afe663e63e9e6c6
Galomortal47 <44421928+Galomortal47@users.noreply.github.com>
2020-07-17 20:24:43 -0300

    as

--- Multyplayer2/code/MultiplayerBrowse.gd
+++ Multyplayer2/code/MultiplayerBrowse.gd
@@ -1,9 +1,14 @@
 extends Multyplayer
 
 func _physics_process(delta):
-	var spliter = string.split("},{")
+	var spliter = string.split("},")
 	spliter = Array(spliter)
-	var piece = spliter[0]+"}"
-	var json = parse_json(piece)
-	get_node("pinglist/RichTextLabel").set_text('a')
+	var string = []
+	for i in spliter:
+		var piece = i + "}"
+		var json = parse_json(piece)
+		string.append(str(json))
+		string.append("\n")
+		string.append("\n")
+	get_node("pinglist/RichTextLabel").set_text(str(string))
 	pass
