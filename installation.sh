ba85a276a35ae1c3331f08460faf6ef450f3e116
Galomortal47 <44421928+Galomortal47@users.noreply.github.com>
2020-08-15 21:22:47 -0300

    fixs 2

--- installation.sh
+++ installation.sh
@@ -1,1 +1,1 @@
-sudo apt install node.js && sudo apt install redis && sudo apt install go-golang && sudo apt install MongoDB && go get "github.com/garyburd/redigo/redis"
+sudo apt install node.js && sudo apt install redis && sudo apt install golang && sudo apt install mongodb && go get "github.com/garyburd/redigo/redis"
